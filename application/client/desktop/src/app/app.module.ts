a6be2c6867356ea7b7328c83c682421a096c0252
naveenlingam <naveenlingam@10decoders.in>
2020-12-24 11:50:26 +0530

    angular test

---
+++ application/client/desktop/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
+import { ApplicationComponent } from './application/application.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SampleComponent,
+    ApplicationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ application/client/desktop/src/app/application/application.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";  
+  
+@Component({  
+     
+    selector: 'my-App',  
+  
+      
+    template: '<h1>Hi {{name}}</h1>'  
+})  
+  
+export class AppComponent {  
+    name: string = "Karthik"  
+}
